adf0c1b3937e8f85d48e4f00161842ca6f9fc6c2
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-24 13:47:25 +0100

    0.2.0

--- lib/src/main/kotlin/io/github/italbytz/adapters/meal/OpenMensaGetMealsCommand.kt
+++ lib/src/main/kotlin/io/github/italbytz/adapters/meal/OpenMensaGetMealsCommand.kt
@@ -4,31 +4,17 @@ import io.github.italbytz.ports.meal.Category
 import io.github.italbytz.ports.meal.GetMealsCommand
 import io.github.italbytz.ports.meal.MealCollection
 import io.github.italbytz.ports.meal.MealQuery
-import kotlinx.coroutines.*
 
 class OpenMensaGetMealsCommand : GetMealsCommand {
 
-    override fun execute(
-        inDTO: MealQuery,
-        successHandler: (success: List<MealCollection>) -> Unit,
-        errorHandler: (error: Throwable) -> Unit
-    ) {
+    override suspend fun execute(inDTO: MealQuery) : List<MealCollection> {
         val dataSource = OpenMensaMealDataSource(inDTO.mensa.toLong(), inDTO.date)
-
-                GlobalScope.launch {
-            val result = kotlin.runCatching {
-                dataSource.retrieveAll()
-            }
-            result.onSuccess {
-                meals ->
-                val groupedMeals = meals.groupBy { it.category }
-                var collections = mutableListOf<MealCollection>()
-                for ((category, meals) in groupedMeals) {
-                    collections.add(OpenMensaMealCollection(category,meals))
-                }
-                successHandler(collections)
-            }
-            result.onFailure(errorHandler)
+        val meals = dataSource.retrieveAll()
+        val groupedMeals = meals.groupBy { it.category }
+        var collections = mutableListOf<MealCollection>()
+        for ((category, meals) in groupedMeals) {
+            collections.add(OpenMensaMealCollection(category,meals))
         }
+        return collections
     }
 }
